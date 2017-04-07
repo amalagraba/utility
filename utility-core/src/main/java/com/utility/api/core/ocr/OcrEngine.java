5df2f1b30f898e7ccccd351e0369bddca5963efc
axel <axel@arteco-consulting.com>
2017-04-07 14:06:53 +0200

    [utility-api] Added OcrEngine Spanish support

--- utility-core/src/main/java/com/utility/api/core/ocr/OcrEngine.java
+++ utility-core/src/main/java/com/utility/api/core/ocr/OcrEngine.java
@@ -8,8 +8,10 @@ import org.bytedeco.javacpp.tesseract;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PreDestroy;
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.UUID;
 
 import static org.bytedeco.javacpp.lept.pixDestroy;
@@ -22,21 +24,31 @@ public class OcrEngine {
     private static final String TMP_PREFIX = "tmp";
 
     private tesseract.TessBaseAPI engine;
-    private OcrEngineProperties properties;
 
     @Autowired
-    public OcrEngine(OcrEngineProperties properties) {
-        this.properties = properties;
+    public OcrEngine(OcrEngineProperties properties) throws OcrException {
         engine = new tesseract.TessBaseAPI();
-    }
-
-
-    public String readImage(byte[] image) throws OcrException {
         // Initialize tesseract-ocr
         if (engine.Init(properties.getConfigPath() , properties.getTrainedLocale()) != 0) {
             throw new OcrException("Could not initialize OcrEngine");
         }
+    }
 
+    @PreDestroy
+    public void stopEngine() {
+        engine.End();
+    }
+
+    /**
+     * Reads the text from an image. Supported formats are PNG, JPEG, TIFF AND BITMAP
+     *
+     * @param image     Image content
+     *
+     * @return Recognized text from the image in UTF-8 encoding
+     *
+     * @throws OcrException when the image cannot be processed
+     */
+    public String readImage(byte[] image) throws OcrException {
         //Create temporary file
         File file = createTemporaryFile(image);
 
@@ -45,20 +57,44 @@ public class OcrEngine {
         engine.SetImage(pix);
 
         // Get OCR result
-        BytePointer outText = engine.GetUTF8Text();
+        String result = getResult(engine.GetUTF8Text());
 
-        if (outText == null) {
-            return "";
-        }
-        String result = outText.getString();
-        engine.End();
         deleteTemporaryFile(file);
-        outText.deallocate();
         pixDestroy(pix);
 
         return result;
     }
 
+    /**
+     * Returns the value of the text recognized by the engine in UTF-8 format
+     *
+     * @param pointer     Engine process result
+     */
+    private String getResult(BytePointer pointer) {
+        if (pointer == null) {
+            return "";
+        }
+        String result;
+        try {
+            result = new String(pointer.getStringBytes(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            result = pointer.getString();
+        }
+        pointer.deallocate();
+
+        return result;
+    }
+
+    /**
+     * Creates a temporary image file to be used by Tesseract to recognize text
+     *
+     * @param image     Image content
+     *
+     * @return A java.io.File object containing the specified image
+     *
+     * @throws OcrException If the file cannot be created properly and therefore no file
+     *         is available for the engine to process
+     */
     private File createTemporaryFile(byte[] image) throws OcrException {
         File file = new File(TMP_PREFIX + UUID.randomUUID());
         try {
@@ -70,6 +106,11 @@ public class OcrEngine {
         return file;
     }
 
+    /**
+     * Deletes a file. Used to delete temporary files created for the engine to process. This method is nullSafe
+     *
+     * @param file  File to be deleted
+     */
     private void deleteTemporaryFile(File file) {
         if (file != null) {
             if (!file.delete()) {
